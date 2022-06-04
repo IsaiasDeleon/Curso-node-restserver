7e955607e5bb52af90ed5a6df7a61c4922eeedae
IsaiasDeleon <deleonsalazaru@gmail.com>
2022-06-03 23:39:45 -0500

    GOOGLE SIGN ADDED

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,8 @@ const { response }= require('express')
 const bcryptjs = require('bcryptjs')
 const Usuario = require('../models/usuario');
 const { jsonWebToken } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
+const { json } = require('express/lib/response');
 const login = async(req, res= response)=>{
     const {correo, password} = req.body;
     try {
@@ -39,6 +41,45 @@ const login = async(req, res= response)=>{
    
 }
 
+const googleSignIn = async ( req, res = response)=>{
+    const { id_token } = req.body;
+   try {
+       const {nombre, img, correo} = await googleVerify(id_token)
+       
+       let usuario = await Usuario.findOne({correo});
+       if(!usuario){
+           //tengo que crearlo
+        const data = {
+            nombre,
+            correo,
+            password:'Se logeo con google',
+            img,
+            google:true,
+            rol:'User_Rol'
+        };
+        usuario = new Usuario(data);
+        await usuario.save();
+       }
+       // si el usuario en DB
+       if( !usuario.estado ){
+           return res.status(401).json({
+               msg:"Hable con el administrador, usuario nloqueado"
+           })
+       }
+       const token = await jsonWebToken(usuario.id)
+    res.json({
+       usuario,token
+    }) 
+   } catch (error) {
+       res.status(400).json({
+           ok: false,
+           msg: "El token no se pudo verificar"
+       })
+   } 
+    
+}
+
 module.exports={
-    login
+    login,
+    googleSignIn
 }
